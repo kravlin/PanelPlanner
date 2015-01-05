561771bedecc26f446d25d61272ec0bd01bb6402
Kravlin <kravlin@gmail.com>
2015-01-05 00:47:41 -0700

    Building out stage.php

--- Stage.php
+++ Stage.php
@@ -1,5 +1,6 @@
+<?php
 defined('ABSPATH') or die("No script kiddies please!");
-
+?>
 <html>
 	<head>
 		<title>Starting Version Demo Page</title>
@@ -7,7 +8,15 @@ defined('ABSPATH') or die("No script kiddies please!");
 	<body>
 
 <?php
-	echo '<p>Hello World</p>';
+	echo '<p>You</p>';
+	echo '<form>'.
+		 'First name: <input type="text" name="panelist_firstname"><br>'.
+		 'Last name: <input type="text" name="panelist_lastname"><br>'.
+		 'Your Email Address: <input type="text" name="panelist_email"><br>'.
+		 'Age:'.
+		 '<input type="text" name="lastname">16-17 <br>'.
+		 '<input type="text" name="lastname">18+<br>'.
+		  '</form>'
 ?>
 	</body>
 </html>
