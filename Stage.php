57824bce10b8ae66d5b96909b9e2b69a4201ab0a
Kravlin <kravlin@gmail.com>
2015-01-04 23:41:23 -0700

    Adding start of the stage.php file

---
+++ Stage.php
@@ -0,0 +1,13 @@
+defined('ABSPATH') or die("No script kiddies please!");
+
+<html>
+	<head>
+		<title>Starting Version Demo Page</title>
+	</head>
+	<body>
+
+<?php
+	echo '<p>Hello World</p>';
+?>
+	</body>
+</html>
