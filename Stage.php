b55a3ff55f15c089bf215a859475f2d58ade3e24
Kravlin <kravlin@gmail.com>
2015-01-05 00:59:03 -0700

    More modification of stage.php

--- Stage.php
+++ Stage.php
@@ -11,9 +11,9 @@
 		 'Last name: <input type="text" name="panelist_lastname"><br>'.
 		 'Your Email Address: <input type="text" name="panelist_email"><br>'.
 		 'Age:'.
-		 '<input type="text" name="lastname">16-17 <br>'.
-		 '<input type="text" name="lastname">18+<br>'.
-		 'Last name: <input type="text" name="lastname"><br>'.
+		 '<input type="radio" name="age">16-17 <br>'.
+		 '<input type="radio" name="age" value="18+">18+<br>'.
+		 '<input type="submit" value="Submit"><br>'.
 		  '</form>'
 ?>
 	</body>
