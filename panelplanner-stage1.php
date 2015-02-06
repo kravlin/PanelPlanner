e1ac12d5d4ad71ee903306766fe3b1d054739aae
Kravlin <kravlin@gmail.com>
2015-02-05 21:56:53 -0700

    trying something

--- panelplanner-stage1.php
+++ panelplanner-stage1.php
@@ -2,8 +2,6 @@
 <head><title> NDK Panel Form - Stage 1 </title>
 <script type="text/Javascript">
     function show_hide(element) {
-        if (document.getElementById(element).style.display == "none"){
-            document.getElementById(element).style.display = "";              
         }
         else{
             document.getElementById(element).style.display = "none";
