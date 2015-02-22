de29f5342f6ff47e12f9a9ee3bb165c55c14dcfc
Kravlin <kravlin@gmail.com>
2015-02-22 01:26:41 -0700

    broke all the shit!

--- PanelPlanner.php
+++ PanelPlanner.php
@@ -24,7 +24,7 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 **/
 
-include dirname(__FILE__) .'/panelplanner-stage1.php';
+//include dirname(__FILE__) .'/panelplanner-stage1.php';
 include dirname(__FILE__) .'/panelplanner-stage2.php';
 include dirname(__FILE__) .'/panelplanner-viewcampaign.php';
 include dirname(__FILE__) .'/panelplanner-viewpanel.php';
