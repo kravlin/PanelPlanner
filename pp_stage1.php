4d5c93efb534f03d9ab3efc610916931f15a67d5
Kravlin <kravlin@gmail.com>
2015-04-03 00:20:18 -0600

    woot woot

--- pp_stage1.php
+++ pp_stage1.php
@@ -1,63 +1,74 @@
-    <form action="'.$_SERVER['REQUEST_URI']." method="post">
+<script type="text/Javascript">
+    function show_hide(element1, element2) {
+        if (document.getElementById(element2).checked){
+            document.getElementById(element1).style.display = "";
+        }
+        else{
+            document.getElementById(element1).style.display = "none";
+        }
+    }
+</script>
 
-    <!-- Begin Panelist -->
+<form action="'.$_SERVER['REQUEST_URI']." method="post">
 
-    <p>
-    First Name (required) <br />
-    <input type="text" name="pp-first-name" pattern="[a-zA-Z0-9 ]+" size="40" />
-    </p>
-    <p>
-    Last Name (required) <br />
-    <input type="text" name="pp-last-name" pattern="[a-zA-Z0-9 ]+" size="40" />
-    </p>
-    <p>
-    Email Address (required) <br />
-    <input type="email" name="pp-email" size="40" />
-    </p>
-    <p>
-    Age (required) <br />
-    <input type="text" name="pp-age" pattern="[0-9]+" size="40" />
-    </p>
+<!-- Begin Panelist -->
 
-    <!-- End Panelist -->
-    
-    <p>
-    <input type="checkbox" checked="false" id="pp-hasCopanelist" onchange="javascript:show_hide(\'CoPanelist\',\'pp-hasCopanelist\');"/> I have a CoPanelist.
-    </p>
+<p>
+First Name (required) <br />
+<input type="text" name="pp-first-name" pattern="[a-zA-Z0-9 ]+" size="40" />
+</p>
+<p>
+Last Name (required) <br />
+<input type="text" name="pp-last-name" pattern="[a-zA-Z0-9 ]+" size="40" />
+</p>
+<p>
+Email Address (required) <br />
+<input type="email" name="pp-email" size="40" />
+</p>
+<p>
+Age (required) <br />
+<input type="text" name="pp-age" pattern="[0-9]+" size="40" />
+</p>
 
-    <!-- Begin Copanelist -->
-    
-    <div id="CoPanelist" style="display: none" >
-    <p>
-    First Name (required) <br />
-    <input type="text" name="pp-first-name2" pattern="[a-zA-Z0-9 ]+" size="40" />
-    </p>
-    <p>
-    Last Name (required) <br />
-    <input type="text" name="pp-last-name2" pattern="[a-zA-Z0-9 ]+" size="40" />
-    </p>
-    <p>
-    Email Address (required) <br />
-    <input type="email" name="pp-email2"  size="40" />
-    </p>
-    <p>
-    Age (required) <br />
-    <input type="text" name="pp-age2" pattern="[0-9]+" size="40" />
-    </p>
-    </div>
+<!-- End Panelist -->
 
-    <!-- End Copanelist / Begin Panel -->
+<p>
+<input type="checkbox" checked="false" id="pp-hasCopanelist" onchange="javascript:show_hide(\'CoPanelist\',\'pp-hasCopanelist\');"/> I have a CoPanelist.
+</p>
 
-    <p>
-    Panel Title (required) <br />
-    <input type="text" name="pp-title" size="40" />
-    </p>
-    Short Panel Description (required) <br />
-    <textarea rows="10" cols="35" name="pp-description"</textarea>';
-    </p>
-    <p>
-    Detailed Panel outline (required) <br />
-    <textarea rows="10" cols="35" name="pp-outline"</textarea>';
-    </p>
-    <p><input type="submit" name="pp-submitted" value="Send"/></p>
-    </form>
+<!-- Begin Copanelist -->
+
+<div id="CoPanelist" style="display: none" >
+<p>
+First Name (required) <br />
+<input type="text" name="pp-first-name2" pattern="[a-zA-Z0-9 ]+" size="40" />
+</p>
+<p>
+Last Name (required) <br />
+<input type="text" name="pp-last-name2" pattern="[a-zA-Z0-9 ]+" size="40" />
+</p>
+<p>
+Email Address (required) <br />
+<input type="email" name="pp-email2"  size="40" />
+</p>
+<p>
+Age (required) <br />
+<input type="text" name="pp-age2" pattern="[0-9]+" size="40" />
+</p>
+</div>
+
+<!-- End Copanelist / Begin Panel -->
+
+<p>
+Panel Title (required) <br />
+<input type="text" name="pp-title" size="40" />
+</p>
+Short Panel Description (required) <br />
+<textarea rows="10" cols="35" name="pp-description"</textarea>';
+</p>
+<p>
+Detailed Panel outline (required) <br />
+<textarea rows="10" cols="35" name="pp-outline"</textarea>';
+</p>
+<p><input type="submit" name="pp-submitted" value="Send"/></p>
+</form>
