d1cdff37e9cb1b122957f57100c6b119a06a7895
Kravlin <kravlin@gmail.com>
2015-04-05 23:50:47 -0600

    FIXES AHOY
    I finally have errors from PHP

--- PanelPlanner.php
+++ PanelPlanner.php
@@ -64,11 +64,11 @@ age int NOT NULL
 )".$charset_collate.";";
 
 	dbDelta( $create_panelists );
-	trigger_error("panelists table should be created", E_USER_WARNING);
+	error_log("panelists table should be created");
 
 	//Create Panels Table
 
-	trigger_error("creating panel table", E_USER_WARNING);
+	error_log("creating panel table");
 	$create_panels = "CREATE TABLE ".$wpdb->prefix."panelPlanner_panels (
 id mediumint(9) AUTO_INCREMENT PRIMARY KEY,
 panelistID mediumint(9) NOT NULL,
@@ -80,7 +80,7 @@ approvalStage int NOT NULL
 )".$charset_collate.";";
 
 	dbDelta( $create_panels );
-	trigger_error("panel table should be created", E_USER_WARNING);
+	error_log("panel table should be created");
 
 	add_option( 'panelplanner_db_version' , $panelplanner_db_version);
 }
@@ -92,7 +92,7 @@ function panel_planner_menu(){
 
 function panel_planner_activate(){
 
-	trigger_error("Starting activation of the plugin", E_USER_WARNING);
+	error_log("Starting activation of the plugin");
 
 	install_database();
 
