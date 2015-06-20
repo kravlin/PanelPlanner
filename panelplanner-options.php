92b90323da24544bd13262de56b752e0698b1e99
Kravlin <Kravlin@gmail.com>
2015-06-20 16:58:17 -0600

    Starting development of the Options page

--- panelplanner-options.php
+++ panelplanner-options.php
@@ -4,7 +4,8 @@ function panel_planner_gen_options_page(){
 	if( !current_user_can( 'manage_options') ){
 		wp_die( __( 'You do not have permission to access this page.') );
 	}
-	
+	global $wpdb;
+	echo "This is the song that gets on everybody's nerves";
 }
 
 ?>
