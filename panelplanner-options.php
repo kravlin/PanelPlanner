3a1cee9eb61cb3cfe7683bd0ca49a156d587f46d
Kravlin <Kravlin@gmail.com>
2015-06-21 20:27:57 -0600

    Derp

--- panelplanner-options.php
+++ panelplanner-options.php
@@ -4,8 +4,23 @@ function panel_planner_gen_options_page(){
 	if( !current_user_can( 'manage_options') ){
 		wp_die( __( 'You do not have permission to access this page.') );
 	}
-	global $wpdb;
 	echo "This is the song that gets on everybody's nerves";
+
+	echo panel_planner_display_panels();
+
+}
+
+function panel_planner_display_panels(){
+	global $wpdb;
+	echo "DERPDERPDERP";
+}
+
+function panel_planner_accept_panel(){
+	echo "SlurpSlurpSlurp";
+}
+
+function panel_planner_deny_panel(){
+	echo "BurpBurpBurp";
 }
 
 ?>
