314a3964e2b417bc557b83f714faf0f50eb650d7
Kravlin <Kravlin@gmail.com>
2015-06-22 23:39:50 -0600

    Stuff goes here

--- panelplanner-options.php
+++ panelplanner-options.php
@@ -78,16 +78,10 @@ function panel_planner_deny_panel(){
 	$tableName = $wpdb->prefix . "panelPlanner_panels";
 	$wpdb->update( 
     	$tableName,
-    	array(
-    		'approvalStage' => '2'
-    	),
-    	array(
-    		'ID' => $panelID
-    	), 
-    	array(
-    		'%d'
-    	), 
-    	array('%d'),
+    	array('approvalStage' => '2'),
+    	array('ID' => $panelID), 
+    	array('%d'), 
+    	array('%d')
     );
 }
 
